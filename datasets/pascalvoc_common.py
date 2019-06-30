2c4aa303e107decf0c953653856346eb801da441
Noureldeen Abdelhameed Ali <43574426+Noureldeen96@users.noreply.github.com>
2019-06-30 02:06:34 +0200

    Update pascalvoc_common.py

--- datasets/pascalvoc_common.py
+++ datasets/pascalvoc_common.py
@@ -22,28 +22,11 @@ from datasets import dataset_utils
 slim = tf.contrib.slim
 
 VOC_LABELS = {
-    'none': (0, 'Background'),
-    'aeroplane': (1, 'Vehicle'),
-    'bicycle': (2, 'Vehicle'),
-    'bird': (3, 'Animal'),
-    'boat': (4, 'Vehicle'),
-    'bottle': (5, 'Indoor'),
-    'bus': (6, 'Vehicle'),
-    'car': (7, 'Vehicle'),
-    'cat': (8, 'Animal'),
-    'chair': (9, 'Indoor'),
-    'cow': (10, 'Animal'),
-    'diningtable': (11, 'Indoor'),
-    'dog': (12, 'Animal'),
-    'horse': (13, 'Animal'),
-    'motorbike': (14, 'Vehicle'),
-    'person': (15, 'Person'),
-    'pottedplant': (16, 'Indoor'),
-    'sheep': (17, 'Animal'),
-    'sofa': (18, 'Indoor'),
-    'train': (19, 'Vehicle'),
-    'tvmonitor': (20, 'Indoor'),
-}
+    'Vehicle': (0, 'Vehicle'),
+    'Dog': (1, 'Animal'),
+    'Bike': (2, 'Vehicle'),
+    'Pedstrain': (3, 'Person'),
+    }
 
 
 def get_split(split_name, dataset_dir, file_pattern, reader,
